effdd75c8686e1aa380c071fde693feb93e6f3b1
Yang seong hyuk <tjdgur1203@naver.com>
2024-11-16 11:02:44 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,9 +1,9 @@
 import geo.untils as utils
 
-a,b =3,4 
-c= utils.pythagoras(a,b)
-print('c =',c)
+a, b = 3, 4 
+c = utils.pythagoras(a,b)
+print('c =', c)
 
-r=10
+r = 10
 area = utils.circle(r)
-print('area =', area )
+print('area =', area)
